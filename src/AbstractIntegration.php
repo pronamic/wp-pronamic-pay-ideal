ac8dbd69195734b14e6d84bc5802f1104ebcf7e3
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 12:00:39 +0100

    Happy 2018.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -3,7 +3,7 @@
 /**
  * Title: iDEAL abstract integration
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
