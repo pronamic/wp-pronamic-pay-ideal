3c5ea2feafdaddd36c9168c15de745f8402af781
Reüel van der Steege <reuel@pronamic.nl>
2022-01-03 14:05:54 +0100

    Happy 2022.

--- src/Statuses.php
+++ src/Statuses.php
@@ -5,7 +5,7 @@ namespace Pronamic\WordPress\Pay\Gateways\IDeal;
 /**
  * Title: iDEAL statuses constants
  * Description:
- * Copyright: 2005-2021 Pronamic
+ * Copyright: 2005-2022 Pronamic
  * Company: Pronamic
  *
  * @author Remco Tolsma
